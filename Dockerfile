ebdd571172b4348dd7710de48fdd6f64dbf6a209
阿七 <yiqiujia@hotmail.com>
2023-06-11 16:58:31 +0800

    test

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM land007/proxy:latest
 
 RUN apt-get update -y --allow-releaseinfo-change && apt-get install -y iptables && apt-get clean
+ENV INIP=192.168.1.1
 ENV LANIP=192.168.1.1
 ADD iptables.sh	/
 RUN chmod +x /iptables.sh
@@ -10,4 +11,8 @@ CMD /task.sh ; /iptables.sh ; /start.sh ; bash
 
 #docker build -t land007/proxy_iptables:latest .
 #> docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t land007/proxy_iptables:latest --push .
-#docker run -it --restart=always -e "username=land007" --privileged --cap-add=NET_ADMIN -e "password=81dc9bdb52d04dc20036dbd8313ed055" -e "LANIP=192.168.1.1" -p 8080:8080 -p 8888:8888 --name proxy land007/proxy:latest
+#docker rm -f proxy_iptables; docker run -it --restart=always -e "username=land007" --privileged --cap-add=NET_ADMIN -e "password=81dc9bdb52d04dc20036dbd8313ed055" -e "INIP=192.168.1.47" -e "LANIP=192.168.1.1" -p 8080:8080 -p 8888:8888 --name proxy_iptables land007/proxy_iptables:latest
+#docker rm -f proxy_iptables; docker run -it --restart=always -e "username=land007" --privileged --cap-add=NET_ADMIN -e "password=81dc9bdb52d04dc20036dbd8313ed055" -e "INIP=192.168.1.47" -e "LANIP=192.168.1.1/24" -p 8080:8080 -p 8888:8888 --name proxy_iptables land007/proxy_iptables:latest
+#curl --proxy http://land007:1234@192.168.1.47:8080 http://192.168.1.1
+# export HTTP_PROXY=http://land007:1234@192.168.1.47:8080
+#curl http://192.168.1.1
--- iptables.sh
+++ iptables.sh
@@ -4,26 +4,50 @@ iptables -F
 iptables -X
 iptables -Z
 iptables -L
+#关闭所有端口
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
-#开启SSH 22端口
-iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 8080 -j ACCEPT
-iptables -A INPUT -p tcp --dport 8888 -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 8888 -j ACCEPT
+#只允许特定ip访问
+if [ "${INIP}" = "" ]
+then
+  echo "INIP is not set"
+  iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+  iptables -A OUTPUT -p tcp --sport 8080 -j ACCEPT
+  iptables -A INPUT -p tcp --dport 8888 -j ACCEPT
+  iptables -A OUTPUT -p tcp --sport 8888 -j ACCEPT
+else
+  echo "INIP is set"
+  #iptables -I INPUT -p tcp --dport 8080 -j DROP
+  #iptables -I INPUT -p TCP --dport 8888 -j DROP
+  for I_INIP in ${INIP}; do
+    echo "iptables -I INPUT -s ${I_INIP} -p tcp --dport 8080 -j ACCEPT"
+    iptables -A INPUT -s ${I_INIP} -p tcp --dport 8080 -j ACCEPT
+    iptables -A OUTPUT -d ${I_INIP} -p tcp --sport 8080 -j ACCEPT
+    echo "iptables -I INPUT -s ${I_INIP} -p tcp --dport 8888 -j ACCEPT"
+    iptables -A INPUT -s ${I_INIP} -p tcp --dport 8888 -j ACCEPT
+    iptables -A OUTPUT -d ${I_INIP} -p tcp --sport 8888 -j ACCEPT
+  done
+fi
 #允许ping
 iptables -A INPUT -p icmp -j ACCEPT
 iptables -A OUTPUT -p icmp -j ACCEPT
 #开启对指定网站的访问
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-#iptables -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -p tcp -d 172.17.0.1/24 -j ACCEPT
-#iptables -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -p tcp -d ${LANIP}/24 -j ACCEPT
-for I_LANIP in ${LANIP}; do
-  echo "iptables -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -p tcp -d ${I_LANIP}/24 -j ACCEPT"
-  iptables -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -p tcp -d ${I_LANIP}/24 -j ACCEPT
-done
+if [ "${LANIP}" = "" ]
+then
+  echo "LANIP is not set"
+else
+  echo "LANIP is set"
+  iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+  #iptables -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -p tcp -d 172.17.0.1/24 -j ACCEPT
+  #iptables -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -p tcp -d ${LANIP}/24 -j ACCEPT
+  for I_LANIP in ${LANIP}; do
+    echo "iptables -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -p tcp -d ${I_LANIP} -j ACCEPT"
+    iptables -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -p tcp -d ${I_LANIP} -j ACCEPT
+  done
+fi
 #允许环回
 iptables -A INPUT -i lo -p all -j ACCEPT
 iptables -A OUTPUT -o lo -p all -j ACCEPT
-
+#输出
+iptables -nv -L
